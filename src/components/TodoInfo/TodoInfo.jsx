4c51ae906b113e799a3bb5701dc7a16eec414093
Andriy <andrijs373@gmail.com>
2025-11-16 13:46:23 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,18 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const className = `TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`;
+
+  return (
+    <div className={className}>
+      <h3 className="TodoInfo__title">{todo.title}</h3>
+      <p className="TodoInfo__id">ID: {todo.id}</p>
+      <p className="TodoInfo__status">
+        Status: {todo.completed ? 'Completed' : 'Pending'}
+      </p>
+
+      {todo.user && <UserInfo user={todo.user} />}
+    </div>
+  );
+};
