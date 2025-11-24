86b0c5667bc5e7c4591856773c407c83f4da6efe
Andriy <andrijs373@gmail.com>
2025-11-24 17:05:09 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { UserInfo } from '../UserInfo/UserInfo';
 
-export const TodoInfo = ({ todo }) => {
-  const className = `TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`;
-
-  return (
-    <div className={className}>
-      <h3 className="TodoInfo__title">{todo.title}</h3>
-      <p className="TodoInfo__id">ID: {todo.id}</p>
-      <p className="TodoInfo__status">
-        Status: {todo.completed ? 'Completed' : 'Pending'}
-      </p>
-
-      {todo.user && <UserInfo user={todo.title} />}
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">CSS</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-    </div>
-  );
-};
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
 export const UserInfo = ({ user }) => (
-  <div className="UserInfo">
-    <p className="UserInfo__name">{user.name}</p>
-    <p className="UserInfo__email">{user.email}</p>
-    {user.company && <p className="UserInfo__company">{user.company.name}</p>}
-
-    <a className="UserInfo" href="mailto:Sincere@april.biz">
-      Leanne Graham
-    </a>
-  </div>
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
